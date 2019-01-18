7d598f52bd810afb2a18695bdbe69b5a6638ac4e
AcevesJJ <josej.acevesb01@hotmail.com>
2019-01-18 11:53:00 -0600

    faltaban jl

---
+++ sacadatos.jl
@@ -0,0 +1,33 @@
+
+
+function sacainfo(entrada)
+    entrada["description"][1].data
+end
+
+function devuelvedatos(h5datos, onchannel)
+    #=
+    Funcion que devuelve los trazos solamente
+    como un dict
+    =#
+    a=()
+    result=Dict{String,Any}()
+    if exists(h5datos, onchannel)
+        aux=read(datos[onchannel])
+        for (keys, values) in aux
+            #println(keys)
+            if keys=="section_00"
+                println("la descripcion de tus barridas de datos son las siguientes")
+                println(values["description"])
+                a=sacainfo(values)
+            end
+            if keys != "description"
+                result[keys]=values["data"]
+            end
+        end
+        else
+        println("no encontre el grupo que pediste")
+        
+    end
+    return (a,result)
+end
+
